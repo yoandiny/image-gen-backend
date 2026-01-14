0b237e0e0bc4a09d2f380072fc30175f3e49b336
Yoan <yoandiny14@gmail.com>
2026-01-14 09:57:09 +0300

    ♻️ refactor(app): set request body size limits for JSON and URL-encoded data

--- app.js
+++ app.js
@@ -6,7 +6,8 @@ import imageRoutes from './routes/image.routes.js';
 
 const app = express();
 
-app.use(express.json());
+app.use(express.json({ limit: "10mb" }));
+app.use(express.urlencoded({ extended: true, limit: "10mb" }));
 app.use(cors({ origin: '*' }));
 
 console.log("Mounting image routes");
