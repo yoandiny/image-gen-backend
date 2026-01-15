70d839b171115fa672b8af6ed03b0c51b1e5e67a
Yoan <yoandiny14@gmail.com>
2026-01-16 02:50:58 +0300

    ♻️ refactor(image.service): update model in generateImageFromImage to use sourceful/riverflow-v2-fast-preview

--- services/image.service.js
+++ services/image.service.js
@@ -114,7 +114,7 @@ export const generateImageFromImage = async (data) => {
   const response = await axios.post(
   'https://openrouter.ai/api/v1/chat/completions',
   {
-  "model": "openai/gpt-5-image-mini",
+  "model": "sourceful/riverflow-v2-fast-preview",
   "modalities": ['image', 'text'],
   "messages": [
       {
